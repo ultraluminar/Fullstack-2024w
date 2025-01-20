3a437f431d979895b5de5d07c9753839981cf7d4
Lukas Klassen <inf4143@hs-worms.de>
2025-01-20 10:59:33 +0100

    feat: elevate user adm into admin

--- database/postgres/2-data.sql
+++ database/postgres/2-data.sql
@@ -22,7 +22,7 @@ SET row_security = off;
 --
 
 COPY public."user" (id, "isAdmin", username, password_hash, email, "createdAt") FROM stdin;
-1	f	admin	$2b$10$dGB1X48Kr2VlnlBlyXXDd.ezbAtp3HnRU026ohqCpErO0UjsZVe7u	admin@example.com	2025-01-19 21:47:16.083377
+1	t	admin	$2b$10$dGB1X48Kr2VlnlBlyXXDd.ezbAtp3HnRU026ohqCpErO0UjsZVe7u	admin@example.com	2025-01-19 21:47:16.083377
 2	f	user1	$2b$10$PWKjAzyH3FzeU5LKa6bYg.wJSPku5.wmEvLrY6nCJLCX3HO1N/h7C	user1@example.com	2025-01-19 21:50:23.078189
 \.
 
